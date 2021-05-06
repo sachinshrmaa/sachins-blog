225d0f56f4eef6630bf0e25d11c67ac9f6ac0368
Sachin Sharma <krisa1826@gmail.com>
2021-05-07 00:26:30 +0530

    revert to initial commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React from 'react'
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom'
 
-
-import NavBar from './Components/NavBar'
-import Footer from './Components/Footer'
-import BlogContainer from './Blog/BlogContainer'
-import BlogDetail from './Blog/BlogDetail'
-import BlogSearch from './Blog/BlogSearch'
-import PrivacyPolicy from './Components/PrivacyPolicy'
-import About from './Components/About'
+import logo from './logo.svg';
+import './App.css';
 
 
 const App = () => {
   return (
-    <Router>
-      <NavBar />
-      <Switch>  
-        
-        <Route exact path="/" component={BlogContainer} />
-        <Route exact path="/search" component={BlogSearch} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/privacy" component={PrivacyPolicy} />
-        <Route exact path="/:id"  component={BlogDetail} />
-        
-      </Switch>
-      <Footer />
-    </Router>
-
-    
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App
