ade73b6e6ec9ff0e914a89f721d90b10ec37448e
Sachin Sharma <sachinkrsharmaa@gmail.com>
2022-11-29 21:10:47 +0530

    v2 beta completed & ready to push

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
-import '../styles/globals.css'
+
 import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.js');
+  }, []);
+
   return(
     <>
       <NavBar/>
