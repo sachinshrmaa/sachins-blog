dcc2a68c6b3b5e293602083eccdd38fb9759fb7e
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-01-17 00:30:21 +0530

    added adsense script

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default function Document() {
             <link rel="preconnect" href="https://fonts.googleapis.com"/><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Lusitana&family=Plus+Jakarta+Sans:wght@300&family=Source+Serif+Pro:wght@300;700;900&display=swap" rel="stylesheet" />
 
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7352636507358273"
+     crossorigin="anonymous"></script>
+     
       <body>
         <Main />
         <NextScript />
