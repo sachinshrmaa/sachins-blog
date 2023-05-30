b2531f6cde3ad06cc3a9eba3cbeede1eb6515462
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-05-30 13:07:34 +0530

    add podcast link in the navbar + updated newsletter link

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,32 +1,55 @@
-
-
 import Link from "next/link";
-import styles from '../styles/Navbar.module.css'
-
+import styles from "../styles/Navbar.module.css";
 
 const NavBar = () => {
-    return (  
-        <nav className="navbar navbar-expand-lg navbar-light bg-white">
-            <div className="container">
-                <Link className="navbar-brand" href='/' id={styles.logo}>Sachins Blog</Link>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <div className="container">
+        <Link className="navbar-brand" href="/" id={styles.logo}>
+          Sachins Blog
+        </Link>
 
-                <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+        <button
+          class="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-link text-dark ms-md-2" href="/about">
+              About
+            </Link>
+            <Link className="nav-link text-dark ms-md-2" href="/bookshelf">
+              Bookshelf
+            </Link>
+            <Link
+              className="nav-link text-dark ms-md-2"
+              href="https://open.spotify.com/show/3wgVx0oPtaBG8adhmjt7CW"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Podcast
+            </Link>
+            <Link
+              className="nav-link text-dark ms-md-2"
+              href="https://thebasics.sachinsblog.in"
+              target="_blank"
+              rel="noreferrer"
+            >
+              MasterClass
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        <Link className="nav-link text-dark ms-md-2" href='/about'>About</Link>
-                        <Link className="nav-link text-dark ms-md-2" href='/bookshelf'>Bookshelf</Link>
-                        {/* <Link className="nav-link text-dark ms-md-2" href='/cohorts'>Cohort</Link> */}
-                        <a className="nav-link text-dark ms-md-2" href='https://thebasics.sachinsblog.in' target="_blank" rel="noreferrer">MasterClass</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-
-    );
-}
- 
 export default NavBar;
--- pages/newsletter.jsx
+++ pages/newsletter.jsx
@@ -1,67 +1,94 @@
 import Head from "next/head";
-import styles from '../styles/Newsletter.module.css'
-
+import styles from "../styles/Newsletter.module.css";
 
 const Newsletter = () => {
-    return (  
-        <div>
-            
-            <Head>
-                <title>Sachins Blog Insider</title>
-                <meta name="description" content="SachinsBlog Insider is a weekly newsletter where every week I set aside an hour or two just to send my insider curated list of content related to self-development, tech, well-being, and more straight into your inbox." />
-                <link rel="icon" href="/sachinsblog.png" />
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>Sachins Blog Insider</title>
+        <meta
+          name="description"
+          content="SachinsBlog Insider is a weekly newsletter where every week I set aside an hour or two just to send my insider curated list of content related to self-development, tech, well-being, and more straight into your inbox."
+        />
+        <link rel="icon" href="/sachinsblog.png" />
+      </Head>
 
+      <div className={styles.heroSection}>
+        <div className="container-md">
+          <h1 className={styles.heroText}>SachinsBlog Insider</h1>
+          <p className={styles.heroPara}>
+            SachinsBlog Insider is a weekly newsletter where every week I set
+            aside an hour or two just to send my insider&apos;s a curated list
+            of content related to self-development, tech, well-being, and more
+            straight into your inbox.
+          </p>
 
-            <div className={styles.heroSection}>
-               
-               <div className="container-md">
-                    <h1 className={styles.heroText}>SachinsBlog Insider</h1>
-                    <p className={styles.heroPara}>SachinsBlog Insider is a weekly newsletter where every week I set aside an hour or two just to send my insider&apos;s a curated list of content related to self-development, tech, well-being, and more straight into your inbox.
-                    </p>
-            
-                
-                    <form className="col-md-7 m-auto" action="https://sachinsblog.us10.list-manage.com/subscribe/post?u=5e93d1e3a83679d27d02d635f&amp;id=13e8fc99a7&amp;f_id=0050cbe5f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_self">
-                        <div className="row mt-4" id="mc_embed_signup_scroll">
-                            <div className="col-md-6 col-12">
-                                <small>First Name</small>
-                                <input type="text" className="form-control" placeholder="First name" name="FNAME" id="mce-FNAME" />
-                                <span id="mce-FNAME-HELPERTEXT" className="helper_text"></span>
-                            </div>
-                            <div className="col-md-6 col-12">
-                                <small>Email *</small>
-                                <input type="email" className="form-control" placeholder="hello@sachinsblog.in" name="EMAIL" id="mce-EMAIL" required />
-                                <span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
-                            </div>
-                        </div>
-                        <div aria-hidden="true">
-                            <input type="text" name="b_5e93d1e3a83679d27d02d635f_13e8fc99a7" tabindex="-1" value="" className="d-none" />
-                        </div>
-                        <div id="mce-responses" className="clear foot">
-                            <div className="response d-none" id="mce-error-response" ></div>
-                            <div className="response d-none" id="mce-success-response"></div>
-                        </div> 
-                        <div class="col mt-2">
-                            <input type="submit" className="form-control bg-dark text-white" value="Join Now" name="subscribe" id="mc-embedded-subscribe" />
-                        </div>
-                    </form>
-                    <br />
-                    <p className={styles.heroPara}> No matter who you are? or what you do? It would be best if you considered subscribing to it.</p>                
-
-        
-
-
-               </div>
-
+          <form
+            className="col-md-7 m-auto"
+            action="https://sachinsblog.us21.list-manage.com/subscribe/post?u=5173dff464536338a3bf9f5d1&amp;id=ab0cc7d456&amp;f_id=00d940e1f0"
+            method="post"
+            id="mc-embedded-subscribe-form"
+            name="mc-embedded-subscribe-form"
+            target="_self"
+          >
+            <div className="row mt-4" id="mc_embed_signup_scroll">
+              <div className="col-md-6 col-12">
+                <small>First Name</small>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  name="FNAME"
+                  id="mce-FNAME"
+                />
+                <span id="mce-FNAME-HELPERTEXT" className="helper_text"></span>
+              </div>
+              <div className="col-md-6 col-12">
+                <small>Email *</small>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="hello@sachinsblog.in"
+                  name="EMAIL"
+                  id="mce-EMAIL"
+                  required
+                />
+                <span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
+              </div>
             </div>
-
-
-            
-            
-
-
+            <div aria-hidden="true">
+              <input
+                type="text"
+                name="b_5173dff464536338a3bf9f5d1_ab0cc7d456"
+                tabindex="-1"
+                value=""
+                className="d-none"
+              />
+            </div>
+            <div id="mce-responses" className="clear foot">
+              <div className="response d-none" id="mce-error-response"></div>
+              <div className="response d-none" id="mce-success-response"></div>
+            </div>
+            <div class="col mt-2">
+              <input
+                type="submit"
+                className="form-control bg-dark text-white"
+                value="Join Now"
+                name="subscribe"
+                id="mc-embedded-subscribe"
+              />
+            </div>
+          </form>
+          <br />
+          <p className={styles.heroPara}>
+            {" "}
+            No matter who you are? or what you do? It would be best if you
+            considered subscribing to it.
+          </p>
         </div>
-    );
-}
- 
+      </div>
+    </div>
+  );
+};
+
 export default Newsletter;
