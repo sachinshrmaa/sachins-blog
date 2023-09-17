b68f1a53d20686a8621c71ece0d25d4d6eeb50ff
Sachin Sharma <sachinkrsharmaa@gmail.com>
2023-09-17 13:43:03 +0530

    style: post page content

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -23,13 +23,13 @@ const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
   return (
-    <div className="max-w-4xl mx-auto py-8 sm:px-6 lg:px-8 min-h-[70vh] ">
+    <div className="max-w-4xl mx-auto px-4 py-8 sm:px-6 lg:px-8 min-h-[70vh] ">
       <img
         src={post.data.thumbnail}
         alt={post.data.title}
         className="rounded-lg mb-4 max-h-[500px] w-full object-center object-cover"
       />
-      <div className="max-w-2xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
+      <div className="max-w-2xl mx-auto py-1 md:py-8 sm:px-6 lg:px-8">
         <small className="latest-post-meta">
           {moment(post.data.date).format("MMMM D, YYYY")}
         </small>
