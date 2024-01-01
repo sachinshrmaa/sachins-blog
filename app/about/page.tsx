d75c64020e154014e5c287af616a0c8dd96875af
sachinshrmaa <krisa1826@gmail.com>
2024-01-01 19:41:56 +0530

    refactor: footer and update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,15 +10,15 @@ export default function About() {
     <div className="max-w-2xl mx-auto px-4 py-8 sm:px-6 lg:px-8 min-h-[70vh]">
       <h1 className="page__heading">About Sachin</h1>
       <p className="mb-3">
-        I am currently working as an Intern for a Cloud-based startup company,
-        where I focuses on works related to frontend development. I&apos;m also
-        pursuing my Bachelor&apos;s degree in Computer Engineering from SIST
+        I am currently working as an Intern for a Cloud-based startup
+        company, where I focuses on works related to frontend development. I&apos;m
+        also pursuing my Bachelor&apos;s degree in Computer Engineering from SIST
         Sikkim Institute of Science and Technology.
       </p>
       <p className="mb-3">
-        Previously, I&apos;ve also completed my Diploma in Computer Engineering
-        with a distinction grade. Apart from studies and work, I actively build
-        and contributes to open-source projects on GitHub.
+        Previously, I&apos;ve also completed my Diploma in Computer Engineering with a
+        distinction grade. Apart from studies and work, I actively build and
+        contributes to open-source projects on GitHub.
       </p>
     </div>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,85 +1,8 @@
-import Link from "next/link";
-
 const Footer = () => {
   return (
-    <footer className="w-full py-6 border-t-[1px]">
-      <div className="grid lg:grid-cols-4 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="px-2 py-1 flex flex-col justify-between">
-          <p id="logo">Sachins blog</p>
-          <div>
-            <p>Made with ❤ by Sachin</p>
-            <p>2023© All Rights Reserved</p>
-          </div>
-        </div>
-        <div>
-          <Link
-            href="/about"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            About
-          </Link>
-          <Link
-            href="https://work.sachinsblog.in/"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Collab
-          </Link>
-          <Link
-            href="https://www.youtube.com/@sachinshrmaa"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Podcast
-          </Link>
-          <Link
-            href="mailto:mail.sachinshrmaa@gmail.com"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Contact
-          </Link>
-        </div>
-        {/* <div>
-          <Link
-            href="#"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Merchandise
-          </Link>
-          <Link
-            href="#"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Podcast
-          </Link>
-          <Link
-            href="#"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Collab
-          </Link>
-          <Link
-            href="#"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            MasterClass
-          </Link>
-        </div>
-        <div>
-          <Link
-            href="#"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Terms
-          </Link>
-          <Link
-            href="#"
-            className="text-blue-900 block px-3 py-1 text-base font-medium hover:text-blue-800"
-          >
-            Privacy Policy 
-          </Link>
-         
-          
-        </div> */}
-      </div>
+    <footer className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6 border-t-[1px]">
+      <p id="logo">Sachins blog</p>
+      <small>© 2021 – present Made with ❤ by Sachin.</small>
     </footer>
   );
 };
